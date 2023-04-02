6e8294b410b4bfcc30e788f9fad18a1093e831e5
CandyVelazco100 <nohemivelazco3@gmail.com>
2023-04-01 18:23:49 -0700

    archivo cambiado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' 
+import App from './app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>hello world</h1>)
+root.render( <App />)
