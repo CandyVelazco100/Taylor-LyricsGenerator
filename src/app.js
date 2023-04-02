2945f2704718a272a0ff0b35a66362381b554c83
CandyVelazco100 <nohemivelazco3@gmail.com>
2023-04-01 22:29:45 -0700

    Nuevos cambios

--- src/app.js
+++ src/app.js
@@ -1,57 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import allQuotes from "./allQuotes";
 import "./style.css";
 
+const Quotes = ({ quote, song, album, handleClick }) => {
+  return (
+    <div className="card">
+      <div id="text">
+        <p>{quote}</p>
+      </div>
+      <div id="song">
+        <p>{song}</p>
+      </div>
+      <div id="album">
+        <p>{album}</p>
+      </div>
+
+      <div className="icon-box">
+        <i className="fa-brands fa-twitter fa-bounce fa-sm"></i>
+        <i className="fa-brands fa-tumblr fa-bounce fa-sm"></i>
+      </div>
+      <footer>
+        <p>Made with ❤️ by Candy</p>
+      </footer>
+    </div>
+  );
+};
+
 const App = () => {
-    const Quotes = () => {
-        const [quote, setQuote] = useState('');
-        const [song, setSong] = useState('');
-        const [album, SetAlbum] = useState('');
-      
-        useEffect(() => {
-          getQuote()
-        }, []);
-      
-        const getQuote = () => {
-              let randomNum = Math.floor(Math.random() * allQuotes.length);
-              const randomQuote = allQuotes[randomNum];
-      
-              setQuote(randomQuote.quote);
-              setSong(randomQuote.song);
-              SetAlbum(randomQuote.album);
-        }
-      
-        const handleClick = () => {
-          getQuote();
-        }
-      
-       return (
-          <div class="card">
-                <div id="text"><p>{quote}</p></div>
-                <div id="song"><p>{song}</p></div>
-                <div id="album"><p>{album}</p></div>
-      
-                <div className="icon-box">
-                <i class="fa-brands fa-twitter fa-bounce fa-sm"></i>
-                <i class="fa-brands fa-tumblr fa-bounce fa-sm"></i>
-                </div>
-                <footer>
-                       <p>Made with ❤️ by Candy</p>
-                </footer>
-             </div>
-        )
-      }
- 
+  const [quote, setQuote] = useState("");
+  const [song, setSong] = useState("");
+  const [album, SetAlbum] = useState("");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    let randomNum = Math.floor(Math.random() * allQuotes.length);
+    const randomQuote = allQuotes[randomNum];
+
+    setQuote(randomQuote.quote);
+    setSong(randomQuote.song);
+    SetAlbum(randomQuote.album);
+  };
+
+  const handleClick = () => {
+    getQuote();
+  };
+
   return (
     <div className="content">
       <div className="card">
         <h2>Random quote</h2>
         <p>Do you want a random quote?</p>
-        <button onClick={handleClick} id="new-quote">New Quote</button>
-        </div>
+        <button onClick={handleClick} id="new-quote">
+          New Quote
+        </button>
+      </div>
       <div id="quote">
-        <Quotes />
-     </div>
+        <Quotes quote={quote} song={song} album={album} handleClick={handleClick} />
+      </div>
     </div>
   );
 };
